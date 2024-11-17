21880433ad2cc1a24ce92c6561ef5411934c7e7f
Tiago Ferreira <thdejesusferreira@gmail.comgit config --listgit config --listgit config --list git config --list--list>
2024-11-17 12:46:21 -0300

    adicionando os cards

---
+++ src/scripts/engine.js
@@ -0,0 +1,18 @@
+const emojis = [
+    "😂","😂","😎","😎","🐱‍🏍","🐱‍🏍",
+    "🎉","🎉","🐱‍🐉","🐱‍🐉","👀","👀",
+    "✨","✨","👍","👍"
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() =>
+ (Math.random() > 0.5?2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").
+    appendChild(box);
+}
